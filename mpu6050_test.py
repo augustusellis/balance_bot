e68d5639308a98d5a9e703e069c00d5ed849f9f6
blakeleiker <blakealeiker@gmail.com>
2018-10-17 13:58:56 -0500

    Wrote tcp_client class

--- mpu6050_test.py
+++ mpu6050_test.py
@@ -2,6 +2,12 @@
 
 import smbus
 from mpu6050 import mpu6050
+from tcp_client import tcp_client
+
+# Start TCP Client
+hostname = '0.0.0.0'
+port = 14150
+my_client = tcp_client(hostname, port)
 
 # Default channel is 1, connected to GPIO pins
 channel = 1
@@ -9,14 +15,16 @@ channel = 1
 # MPU-6050 defaults to address asdf
 address = 0x68
 
-# What are register addresses?
-reg_write_dac = 0x40
+reg_write_dac = 0x40 # Currently unnecessary
 
 # Initialize I2C
 bus = smbus.SMBus(channel)
 
 mpu = mpu6050(address)
 
+
 while True:
-    # print(mpu.get_accel_data())
-    print(mpu.get_gyro_data())
+    accel_data = mpu.get_accel_data()
+    my_client.send_msg(accel_data)
+    print(accel_data)
+    #print(mpu.get_gyro_data())
