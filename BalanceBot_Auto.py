1f783163023ddfe4a9b406e068ff4b29be82c848
augustusellis <augustusellis@gmail.com>
2018-12-04 22:39:42 -0600

    Updated Comments and removed irrelevant (unused and commented out) code. Now with the autorun program also edited.

--- BalanceBot_Auto.py
+++ BalanceBot_Auto.py
@@ -85,59 +85,45 @@ try:
     time.sleep(led_wait)
 
 
-    M1_pos = motor1.get_pos()
-    M2_pos = motor2.get_pos()
+    M1_pos_deck = deque([0, 0], 25)
+    M2_pos_deck = deque([0, 0], 25)
 
-    M1_diff_deck = deque([0, 0], 25)
-    M2_diff_deck = deque([0, 0], 25)
-
-    t = time.time()
-    current_time = t
 
     r = 0
     count= 0
     xref = 0
 
-    time_start = time.time()
+    t = time.time()
+    current_time = t
+    time_start = t
     while True:
         count = count + 1
 
-        ## Do Timing
+        # Do Timing
         previous_time = current_time
         current_time = time.time()
         t = t + period
 
-        ## Get Data
-        # Raw Data
+        # Get Data
         gx, gy, gz, _, ax, ay, az = mpu.get_all_data()
 
-        #M1_pos_prev = M1_pos
-        #M2_pos_prev = M2_pos
         M1_pos = motor1.get_pos()
         M2_pos = motor2.get_pos()
 
-        M1_diff_deck.append(M1_pos)
-        M2_diff_deck.append(M2_pos)
+        M1_pos_deck.append(M1_pos)
+        M2_pos_deck.append(M2_pos)
 
-
-        #RPS_M1 = (M1_diff_deck[-1] - M1_diff_deck[0])/ ((float(len(M1_diff_deck))-1)*period) #(M1_pos - M1_pos_prev)/(period) #np.mean(M1_diff_deck)/period
-        #RPS_M2 = (M2_diff_deck[-1] - M2_diff_deck[0])/ ((float(len(M2_diff_deck))-1)*period) #(M1_pos - M1_pos_prev)/(period)
-
-        ## Update Data Filter
+        # Update Data Filter
         cfilt.update([gx,gy,gz,ax,ay,az], current_time-previous_time)
         if abs(cfilt.rollangle) > 35:
-           break
-        #omega_raw.append((cfilt.accel_angle,gx,gy,gz))
+            print('Angle too high, Breaking Loop: {}'.format(cfilt.rollangle))
+            break
 
-        ## Update Control Signals
-        x = ((np.mean(M1_diff_deck)+np.mean(M2_diff_deck))/2)*10
+        # Update Control Signals
+        x = ((np.mean(M1_pos_deck)+np.mean(M2_pos_deck))/2)*10
         error = (r-cfilt.rollangle) + 0.025*copysign(1,x-xref)*min([abs(x - xref), 2.5])
         balance_controller.update(error)
-        #M1_controller.update( balance_controller.u - RPS_M1)
-        #M2_controller.update( balance_controller.u - RPS_M2)
 
-        #M1_controller.update( balance_controller.u - RPS_M1) #160/60 - RPS_M1)
-        #M2_controller.update( balance_controller.u - RPS_M2) #160/60 - RPS_M2)
         # Send Control Signal to Motors
         motor1.set_duty_cycle(balance_controller.u)
         motor2.set_duty_cycle(balance_controller.u)
@@ -152,42 +138,22 @@ try:
             pi.write(led2, 1)
 
         if (count % 40) == 0:
-            #print('{:>6.3F}'.format(RPS_M1*60))
-            #print("ERROR RPM_M1: {:<+10.2F}, ERROR RPM_M2: {:<+10.2F}".format( 100 - RPS_M1*60, 100 - RPS_M2*60))
-            print("count:: {:d}  :::: theta: {: >+7.2F}, uP: {: >+7.2F}, uI: {: >+7.2F}, ud: {: >+6.1F}, u: {: >+6.1F}, X: {: >+6.1F}".format(count, cfilt.rollangle, balance_controller.eP*balance_controller.kP, balance_controller.eI*balance_controller.kI, balance_controller.eD*balance_controller.kD, balance_controller.u, x))
-            #print('Theta: {0:.1f},  u: {1:.1f}'.format(cfilt.rollangle,balance_controller.u))
+            #print("count:: {:d}  :::: theta: {: >+7.2F}, uP: {: >+7.2F}, uI: {: >+7.2F}, ud: {: >+6.1F}, u: {: >+6.1F}, X: {: >+6.1F}".format(count, cfilt.rollangle, balance_controller.eP*balance_controller.kP, balance_controller.eI*balance_controller.kI, balance_controller.eD*balance_controller.kD, balance_controller.u, x))
             #print('Accel: {: >+7.2F}, Gyro: {: >+7.2F}, Pitch: {: >+7.2F}, gx: {: >+7.2F},, gy: {: >+7.2F},, gz: {: >+7.2F},, ax: {: >+7.2F},, ay: {: >+7.2F},, az: {: >+7.2F},'.format(cfilt.accel_angle,cfilt.gyro_angle,cfilt.rollangle,gx,gy,gz,ax,ay,az))
-            #print('PER: {}'.format((current_time-time_start)/count))
             pass
-        #if (count % 25) == 0:
-        #    if r-theta > 0:
-        #        print("\033[92m theta: {0: 0.3F} er: {1: 0.3F} up: {2: 0.3F}, uI: {3: 0.3F}, uD: {4: 0.3F} \033[0m".format(theta, controller.eP, controller.eP*controller.kP, controller.eI*controller.kI, controller.eD*controller.kD))
-        #    else:
-        #        print("\033[91m theta: {0: 0.3F} er: {1: 0.3F} up: {2: 0.3F}, uI: {3: 0.3F}, uD: {4: 0.3F} \033[0m".format(theta, controller.eP, controller.eP*controller.kP, controller.eI*controller.kI, controller.eD*controller.kD))
 
-        #omega_raw.append((cfilt.rollangle,0))
-        #theta_raw.append(cfilt.rollangle)
-        #u_raw.append(balance_controller.u)
-        #sim_time.append(current_time)
         # Sleep for Remaining Loop Time
         time.sleep(max(0, t-time.time()))
 
 
-    #print(tuple(np.mean(omega_raw,axis=0)))
-
     print('PER: {}'.format((time.time() - time_start)/count))
-    #print(np.mean(theta_raw))
-    #imu.stop_imu()
-    print('Angle too high, turning off motors: {}'.format(cfilt.rollangle))
     motor1.set_duty_cycle(0)
     motor2.set_duty_cycle(0)
     pi.write(led1, 0)
     pi.write(led2, 0)
 
 
-
 except KeyboardInterrupt:
-    #imu.stop_imu()
     print('PER: {}'.format((time.time() - time_start)/count))
     print('Turning off motors.')
     motor1.set_duty_cycle(0)
@@ -198,7 +164,8 @@ except KeyboardInterrupt:
 
 
 except IOError:
-    #imu.stop_imu()
     print('IOERROR: Turning off motors.')
     motor1.set_duty_cycle(0)
     motor2.set_duty_cycle(0)
+    pi.write(led1, 0)
+    pi.write(led2, 0)
